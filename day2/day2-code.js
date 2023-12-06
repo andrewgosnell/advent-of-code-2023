62e3aa0042f9a4d58a80303cd4d7dbfc184e6d3a
Andrew Gosnell <andrew.gosnell@zapier.com>
2023-12-05 23:33:49 -0800

    day2 complete

--- day2/day2-code.js
+++ day2/day2-code.js
@@ -38,6 +38,40 @@ const transformLines = (line) => {
   return gameNumber;
 };
 
+const day2Function2 = () => {
+  const fileString = readFileSync(filePath, "utf8");
+  const lineArray = fileString.split("\n");
+
+  const filtered = lineArray.map(transformLines2);
+  const sum = filtered.reduce((sum, value) => sum + value);
+
+  return sum;
+};
+
+const transformLines2 = (line) => {
+  let game = { red: 0, green: 0, blue: 0 };
+
+  const splitGame = line.split(": ");
+  const cubes = splitGame[1]
+    .replaceAll(";", ",")
+    .replaceAll(", ", ",")
+    .split(",");
+
+  for (str of cubes) {
+    const values = str.split(" ");
+    const number = Number.parseInt(values[0]);
+    const color = values[1];
+
+    if (number > game[color]) {
+      game[color] = number;
+    }
+  }
+
+  const power = game["red"] * game["green"] * game["blue"];
+  return power;
+};
+
 module.exports = {
   day2Function,
+  day2Function2,
 };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // main file
 
 const { day1Function } = require("./day1/day1-code");
-const { day2Function } = require("./day2/day2-code");
+const { day2Function, day2Function2 } = require("./day2/day2-code");
 
-let output = day2Function();
+let output = day2Function2();
 console.log(output);
