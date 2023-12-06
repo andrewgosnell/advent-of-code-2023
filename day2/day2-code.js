682eea5e2b51c39f3d4f8f1924c5ad10c2b0ed54
Andrew Gosnell <andrew.gosnell@zapier.com>
2023-12-05 23:05:21 -0800

    part 1 complete

--- day2/day2-code.js
+++ day2/day2-code.js
@@ -5,9 +5,37 @@ const day2Function = () => {
   const fileString = readFileSync(filePath, "utf8");
   const lineArray = fileString.split("\n");
 
-  console.log(lineArray);
+  const filtered = lineArray.map(transformLines);
+  const sum = filtered.reduce((sum, value) => sum + value);
 
-  return "hello world";
+  return sum;
+};
+
+const transformLines = (line) => {
+  const splitGame = line.split(": ");
+  const cubes = splitGame[1]
+    .replaceAll(";", ",")
+    .replaceAll(", ", ",")
+    .split(",");
+
+  for (str of cubes) {
+    const values = str.split(" ");
+    if (values[0] > 12 && values[1] === "red") {
+      return 0;
+    }
+    if (values[0] > 13 && values[1] === "green") {
+      return 0;
+    }
+    if (values[0] > 14 && values[1] === "blue") {
+      return 0;
+    }
+  }
+
+  const splitNumber = splitGame[0].replaceAll(" ", ":").split(":");
+  const gameNumber = Number.parseInt(splitNumber[1]);
+  console.log(gameNumber);
+  console.log(cubes);
+  return gameNumber;
 };
 
 module.exports = {
