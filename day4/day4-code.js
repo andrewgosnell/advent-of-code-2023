791c5ebaf379951a41fb86e751318fef9833b58f
Andrew Gosnell <andrew.gosnell@zapier.com>
2023-12-09 22:37:01 -0800

    day4 part1

--- day4/day4-code.js
+++ day4/day4-code.js
@@ -1,12 +1,25 @@
 const { readFileSync } = require("node:fs");
-//const filePath = "./day3/input.txt";
-const filePath = "./day3/test.txt";
+const filePath = "./day4/input.txt";
+//const filePath = "./day4/test.txt";
 
 const day4Function = () => {
   const fileString = readFileSync(filePath, "utf8");
-  const lineArray = fileString.split("\n");
+  const lineArray = fileString.split("\n").map(calculateLine);
 
-  return "hello world";
+  return lineArray.reduce((sum, value) => sum + value);
+};
+
+const calculateLine = (line, index, arr) => {
+  const dropBeginning = line.split(": ");
+  const fullCard = dropBeginning[1].trim().replaceAll("  ", " ").split(" | ");
+
+  const winners = fullCard[0].split(" ");
+  const myCard = fullCard[1].split(" ");
+
+  const wins = myCard.filter((number) => winners.includes(number));
+  const value = wins.length > 0 ? 2 ** (wins.length - 1) : 0;
+
+  return value;
 };
 
 module.exports = {
